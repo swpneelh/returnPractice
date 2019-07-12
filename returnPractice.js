b7bea567f82bb2b9c9f3b0a07d1484f7fd739bf3
swpneelhASC6 <swpneel2003@gmail.com>
2019-07-12 09:46:40 -0400

    Create Func with Return value

---
+++ returnPractice.js
@@ -0,0 +1,9 @@
+function sumFunc(){
+//return syntax
+return "Hi Planet";
+return 5; 
+} // by default all JS func returns a value, if not stated it returns undefined 
+
+const result = sumFunc();
+
+console.log(result);
